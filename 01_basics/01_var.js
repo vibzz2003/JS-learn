cf3898d9447a61e0ecb717878d40a3905e5944db
vibzz2003 <500093004@stu.upes.ac.in>
2024-04-22 19:22:11 +0530

    var and datattype

---
+++ 01_basics/01_var.js
@@ -0,0 +1,16 @@
+const accountId = 123234
+let accountEmail = "rminocha01@gmail.com"
+var password = "Vibhor"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 3
+// console.log(accountId) //not allowed
+
+//prefer not to use var, because issue in scope and functinal scope
+
+accountEmail = "ranyak@gmail.com"
+password = 'ranya'
+accountCity = "bengaluru"
+
+console.table([accountId, accountEmail, password, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) - used when binding with frontend, we are using nodejs not browser
+
+console.log(3+3)
+console.log("Vibhor") //code readability should be high
+
+let name = "ranya" //string datatype
+let age = 21 //int datatype
+let isLoggedIn = false //bool datatype
+//null datatype
+//undefined datatype
+//bigint
+let temp = null
+//symbol -> when working with react, figma tools unique
+//object 
+
+console.log(typeof "Vibhor")
+
+console.log(typeof null)
+//null is an object
+
+//undefined ka type bhi undefined hai
