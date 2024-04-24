81e9e7ab9a887ad5053904a152e1dddc59bac669
vibzz2003 <500093004@stu.upes.ac.in>
2024-04-24 17:51:55 +0530

    conversions

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,17 @@
+let score = "33"
+let score1 = "33abc"
+let score2 = null
+console.log(score)
+console.log(typeof score)
+console.log(typeof(score1))
+
+let valueinnumber = Number(score2)
+console.log(valueinnumber)
+console.log( typeof valueinnumber)
+
+
+let IsLogged = 1
+console.log(typeof IsLogged)
+let LoggedIn = Boolean(IsLogged)
+console.log(LoggedIn)
+console.log(typeof(LoggedIn))
